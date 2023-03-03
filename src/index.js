a01446b40ef0de3adce5b8c9a1eb2ff50dd7703e
huyhoangm2 <hoangken760@gmail.com>
2023-03-03 20:43:28 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './app/store';
+import TodoApp from './features/todo';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
